f5359090ed99a217f23f77cd703623e44064215d
ZockerAxel <zocker7899@gmail.com>
2025-04-09 14:12:08 +0200

    Bump Cache Version to 10

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const pageDomain = "ts5viewer.endercentral.eu";
-const cacheVersion = 9;
+const cacheVersion = 10;
 const beta = pageDomain !== location.hostname;
 const currentCacheKey = `v${cacheVersion}${beta ? "-beta" : ""}`;
 
